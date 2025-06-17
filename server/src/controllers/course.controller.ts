8051a8cf80b3d9d70f2b0f3d32b023fecba25af0
Mahesh Moholkar <maheshmoholkar171@gmail.com>
2025-06-17 19:54:31 +0530

    add edit course

--- server/src/controllers/course.controller.ts
+++ server/src/controllers/course.controller.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import courseModel from "../models/course.model";
 import { CatchAsyncError } from "../middlewares/catchAsyncErrors";
 import ErrorHandler from "../helpers/ErrorHandler";
-import { uploadFile, BUCKETS } from "../utils/minio";
+import { uploadFile, BUCKETS, deleteFile } from "../utils/minio";
 import { createCourse } from "../services/course.service";
 
 export const uploadCourse = CatchAsyncError(
@@ -20,13 +20,26 @@ export const uploadCourse = CatchAsyncError(
       if (typeof data.prerequisites === "string") {
         data.prerequisites = JSON.parse(data.prerequisites);
       }
-      if (
-        data.courseData &&
-        data.courseData[0]?.links &&
-        typeof data.courseData[0].links === "string"
-      ) {
-        data.courseData[0].links = JSON.parse(data.courseData[0].links);
+
+      // Transform flattened courseData into array of objects
+      const courseDataArray = [];
+      let index = 0;
+      while (data[`courseData[${index}].title`]) {
+        const courseDataItem = {
+          title: data[`courseData[${index}].title`],
+          description: data[`courseData[${index}].description`],
+          videoUrl: data[`courseData[${index}].videoUrl`],
+          videoSection: data[`courseData[${index}].videoSection`],
+          videoLength: data[`courseData[${index}].videoLength`],
+          links:
+            typeof data[`courseData[${index}].links`] === "string"
+              ? JSON.parse(data[`courseData[${index}].links`])
+              : data[`courseData[${index}].links`],
+        };
+        courseDataArray.push(courseDataItem);
+        index++;
       }
+      data.courseData = courseDataArray;
 
       // Handle thumbnail upload if present
       if (req.file) {
@@ -41,10 +54,98 @@ export const uploadCourse = CatchAsyncError(
         return next(new ErrorHandler("Please upload a course thumbnail", 400));
       }
 
-      console.log(data);
-
       // Create course
       createCourse(data, res, next);
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+export const editCourse = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body;
+      const courseId = req.params.id;
+
+      // Validate course ID
+      if (!courseId) {
+        return next(new ErrorHandler("Course ID is required", 400));
+      }
+
+      // Check if course exists
+      const existingCourse = await courseModel.findById(courseId);
+      if (!existingCourse) {
+        return next(new ErrorHandler("Course not found", 404));
+      }
+
+      // Parse stringified JSON values
+      if (typeof data.tags === "string") {
+        data.tags = JSON.parse(data.tags);
+      }
+      if (typeof data.benefits === "string") {
+        data.benefits = JSON.parse(data.benefits);
+      }
+      if (typeof data.prerequisites === "string") {
+        data.prerequisites = JSON.parse(data.prerequisites);
+      }
+
+      // Transform flattened courseData into array of objects
+      const courseDataArray = [];
+      let index = 0;
+      while (data[`courseData[${index}].title`]) {
+        const courseDataItem = {
+          title: data[`courseData[${index}].title`],
+          description: data[`courseData[${index}].description`],
+          videoUrl: data[`courseData[${index}].videoUrl`],
+          videoSection: data[`courseData[${index}].videoSection`],
+          videoLength: data[`courseData[${index}].videoLength`],
+          links:
+            typeof data[`courseData[${index}].links`] === "string"
+              ? JSON.parse(data[`courseData[${index}].links`])
+              : data[`courseData[${index}].links`],
+        };
+        courseDataArray.push(courseDataItem);
+        index++;
+      }
+      if (courseDataArray.length > 0) {
+        data.courseData = courseDataArray;
+      }
+
+      // Handle thumbnail upload if present
+      if (req.file) {
+        // Delete old thumbnail if it exists
+        if (existingCourse.thumbnail?.public_id) {
+          await deleteFile(
+            existingCourse.thumbnail.public_id,
+            BUCKETS.COURSE_THUMBNAILS
+          );
+        }
+
+        const fileUrl = await uploadFile(
+          req.file.buffer,
+          req.file.originalname,
+          req.file.mimetype,
+          BUCKETS.COURSE_THUMBNAILS
+        );
+        data.thumbnail = {
+          public_id: req.file.originalname,
+          url: fileUrl,
+        };
+      }
+
+      const course = await courseModel.findByIdAndUpdate(
+        courseId,
+        {
+          $set: data,
+        },
+        { new: true }
+      );
+
+      res.status(200).json({
+        success: true,
+        course,
+      });
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 500));
     }
--- server/src/routes/course.route.ts
+++ server/src/routes/course.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { uploadCourse } from "../controllers/course.controller";
+import { editCourse, uploadCourse } from "../controllers/course.controller";
 import { isAuthenticated, authorizeRoles } from "../middlewares/auth";
 import upload from "../middlewares/upload";
 
@@ -12,6 +12,14 @@ courseRouter.post(
   authorizeRoles("admin"),
   upload.single("thumbnail"),
   uploadCourse
+);
+
+courseRouter.put(
+  "/edit-course/:id",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  upload.single("thumbnail"),
+  editCourse
 );
 
 export default courseRouter;
